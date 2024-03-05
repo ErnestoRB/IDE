05f144769719389de28c986964cdec451e90f03b
Ernesto Ramírez <ernestorb_@outlook.com>
2024-03-05 17:09:18 -0600

    Agregar boton de compilado general

--- src/side/items/CompilerButton.tsx
+++ src/side/items/CompilerButton.tsx
@@ -1,47 +1,56 @@
-import { Menu } from '@headlessui/react';
-import { BsPlayCircle } from 'react-icons/bs';
+import { Menu } from "@headlessui/react";
+import { BsPlayCircle } from "react-icons/bs";
 import { BsAlphabetUppercase } from "react-icons/bs";
 import { MdMenuBook } from "react-icons/md";
 import { BsGear } from "react-icons/bs";
+import { GoChevronDown } from "react-icons/go";
 
 export function CompilerButton() {
   return (
-    <Menu as="div" className="relative">
-      <Menu.Button className="w-10 h-10 grid place-items-center text-white bg-stone-900 hover:bg-black">
+    <div className="flex text-white bg-stone-900 hover:bg-black">
+      <button className="grid place-items-center w-10 h-10">
         <BsPlayCircle size={20} />
-      </Menu.Button>
-      <Menu.Items className="absolute top-10 left-0 z-10 grid gap-2 justify-center place-items-center text-white bg-stone-900">
-        <Menu.Item>
-          {({ active }) => (
-            <a
-              className={`${active && 'hover:bg-black'} w-full h-full px-3 py-2 gap-4 place-items-center flex justify-between`}
-              href=""
-            >
-              Sintático <BsGear />
-            </a>
-          )}
-        </Menu.Item>
-        <Menu.Item>
-          {({ active }) => (
-            <a
-              className={`${active && 'hover:bg-black'} w-full h-full px-3 py-2 gap-4 place-items-center flex justify-between`}
-              href=""
-            >
-              Semántico <MdMenuBook />
-            </a>
-          )}
-        </Menu.Item>
-        <Menu.Item>
-          {({ active }) => (
-            <a
-            className={`${active && 'hover:bg-black'} w-full h-full px-3 py-2 gap-4 place-items-center flex justify-between`}
-              href=""
-            >
-              Léxico <BsAlphabetUppercase />
-            </a>
-          )}
-        </Menu.Item>
-      </Menu.Items>
-    </Menu>
+      </button>
+      <Menu as="div" className="relative">
+        <Menu.Button className="grid place-items-center h-full bg-black">
+          <GoChevronDown></GoChevronDown>
+        </Menu.Button>
+        <Menu.Items className="absolute top-10 left-0 z-10 grid gap-2 justify-center place-items-center text-white bg-stone-900">
+          <Menu.Item>
+            {({ active }) => (
+              <a
+                className={`${
+                  active && "hover:bg-black"
+                } w-full h-full px-3 py-2 gap-4 place-items-center flex justify-between`}
+              >
+                Sintático <BsGear />
+              </a>
+            )}
+          </Menu.Item>
+          <Menu.Item>
+            {({ active }) => (
+              <a
+                className={`${
+                  active && "hover:bg-black"
+                } w-full h-full px-3 py-2 gap-4 place-items-center flex justify-between`}
+              >
+                Semántico <MdMenuBook />
+              </a>
+            )}
+          </Menu.Item>
+          <Menu.Item>
+            {({ active }) => (
+              <a
+                className={`${
+                  active && "hover:bg-black"
+                } w-full h-full px-3 py-2 gap-4 place-items-center flex justify-between`}
+              >
+                Léxico <BsAlphabetUppercase />
+              </a>
+            )}
+          </Menu.Item>
+        </Menu.Items>
+      </Menu>
+    </div>
   );
 }
