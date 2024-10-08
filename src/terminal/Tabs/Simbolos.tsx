f03ecbf1aae04c005191d9d0a635a8c5c6d646b9
Iker Jiménez <ender12as@gmail.com>
2024-10-07 23:38:30 -0600

    Cambios de diseño en tabla de simbolos

--- src/terminal/Tabs/Simbolos.tsx
+++ src/terminal/Tabs/Simbolos.tsx
@@ -4,37 +4,37 @@ export function Simbolos() {
   const result = useFileStore((f) => f.semanticoResult);
 
   return (
-    <div className="w-full h-full bg-stone-700 text-white grid">
-      <div className="w-full grid grid-cols-6">
-        <div className="bg-stone-900">Nombre</div>
-        <div className="bg-stone-900">Tipo</div>
-        <div className="bg-stone-900">Valor</div>
-        <div className="bg-stone-900">Definicion</div>
-        <div className="bg-stone-900">Ubicacion en memoria</div>
-        <div className="bg-stone-900">Usos</div>
+    <div className="w-full h-full bg-stone-700 text-white grid text-center">
+      <div className="w-full grid grid-cols-6 border border-white">
+        <div className="bg-stone-900 border">Nombre</div>
+        <div className="bg-stone-900 border">Tipo</div>
+        <div className="bg-stone-900 border">Valor</div>
+        <div className="bg-stone-900 border">Definicion</div>
+        <div className="bg-stone-900 border">
+          Ubicacion en memoria
+        </div>
+        <div className="bg-stone-900 border border-white">Lineas de Uso</div>
       </div>
       {result &&
         Object.entries(result[0]).map(([name, data]) => {
           return (
             <div
               key={name}
-              className="w-full bg-stone-700 h-full grid grid-cols-6"
+              className="w-full bg-stone-700 h-full grid grid-cols-6 border"
             >
-              <div>{name}</div>
-              <div>{data.typ}</div>
-              <div>
+              <div className="border">{name}</div>
+              <div className="border">{data.typ}</div>
+              <div className="border">
                 {data.value == null
                   ? "No establecido"
                   : Object.values(data.value)[0]}
               </div>
-              <div>
-                Linea {data.declaration.lin}, Columna {data.declaration.col}
+              <div className="border">
+                Linea {data.declaration.lin}
               </div>
-              <div>{data.mem_location}</div>
-              <div>
-                {data.usages
-                  .map((u) => `(${u.cursor.lin}, ${u.cursor.col})`, "")
-                  .join("  | ")}
+              <div className="border">{data.mem_location}</div>
+              <div className="border">
+                {data.usages.map((u) => `${u.cursor.lin}`, "").join("  | ")}
               </div>
             </div>
           );
