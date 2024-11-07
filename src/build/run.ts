717f7ee769a4425fb1c2cae8485e4b7c11b1dd01
Ernesto Ramírez <ernestorb_@outlook.com>
2024-11-07 16:43:58 -0600

    Permitir ejecución en terminal activa

--- src/build/run.ts
+++ src/build/run.ts
@@ -1,7 +1,13 @@
+import { useFileStore } from "../stores/files";
 import { useLayoutStore } from "../stores/layout";
 import { useTerminalStore } from "../stores/terminal";
 
 export function run() {
+  const code = useFileStore.getState().generatedCode;
+  if (!code) {
+    console.error("No code to run");
+    return;
+  }
   let activeTerminal = useTerminalStore.getState().activeTerminal;
   const terminals = useTerminalStore.getState().terminals;
   if (!activeTerminal) {
@@ -15,5 +21,14 @@ export function run() {
   }
   useLayoutStore.getState().terminalPanelRef?.expand(); // Mostrar barra inferior de terminal
   useLayoutStore.getState().setActiveTab(0); // mostrar tab de temrinal
-  activeTerminal.ttyFunctions.sendDataShell("echo 'Hello world!'\n");
+  const shell = activeTerminal.shell;
+  console.log("Running code in shell", shell);
+  activeTerminal.ttyFunctions.sendDataShell("vainilla-machine run-stdin\n");
+  activeTerminal.ttyFunctions.sendDataShell(code + "\n");
+  activeTerminal.ttyFunctions.sendDataShell("\x04");
+
+  // if (shell.includes("bash")) {
+  // } else if (shell.includes("pwsh")) {
+  //   activeTerminal.ttyFunctions.sendDataShell("echo 'Hello world!'\n");
+  // }
 }
